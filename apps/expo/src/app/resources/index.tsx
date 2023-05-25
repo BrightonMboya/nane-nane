116c5ff56b9179667b9c65c9e86879563c9628ef
BrightonMboya <b.mboya@alustudent.com>
2023-05-25 14:59:52 +0200

    added some styles on resources page

--- apps/expo/src/app/resources/index.tsx
+++ apps/expo/src/app/resources/index.tsx
@@ -1,12 +1,13 @@
 import {
-  Button,
   ScrollView,
   Text,
   Touchable,
   TouchableHighlight,
+  TouchableOpacity,
   View,
 } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
+import * as Linking from "expo-linking";
 import { Stack, useRouter } from "expo-router";
 
 import { api } from "~/utils/api";
@@ -24,17 +25,17 @@ const JobPost: React.FC<{
 }> = ({ post }) => {
   const router = useRouter();
   return (
-    <TouchableHighlight
+    <TouchableOpacity
       onPress={() => {
         router.push(`/resources/${post.id}`);
       }}
     >
       <View className="w-[350px] gap-1 border-b-[1px] border-b-gray-300 pb-3 pt-5">
-        <Text className="text-xl font-medium">{post.title}</Text>
+        <Text className="text-lg font-medium">{post.title}</Text>
         <Text className="text-[15px]">{post.company}</Text>
         <Text className="text-gray-700">4 days ago</Text>
       </View>
-    </TouchableHighlight>
+    </TouchableOpacity>
   );
 };
 
@@ -44,17 +45,42 @@ const Index = () => {
     <SafeAreaView className="relative bg-[#f2f2f2] ">
       <ScrollView>
         <Stack.Screen options={{ title: "Profile Page", headerShown: false }} />
-        <View className="mt-5 flex flex-col items-center">
-          <Text className="font-medium">
-            Want to help the ALU community get hired?
-          </Text>
-          <TouchableHighlight>
-            <Text className="rounded-base mt-3 w-[170px] bg-indigo-500 px-1  py-2  text-center text-lg text-white">
-              Share Opportunity
+        <View className="flex flex-col items-center">
+          <View className="mt-5 flex h-[100px] w-[300px] flex-col items-center rounded-md  shadow-md">
+            <Text className="font-medium">
+              Want to help the ALU community get hired?
             </Text>
-          </TouchableHighlight>
+            <TouchableHighlight>
+              <Text className="rounded-base mt-3 w-[170px] bg-indigo-500 px-1  py-2  text-center text-base text-white">
+                Share Opportunity
+              </Text>
+            </TouchableHighlight>
+          </View>
+
+          <View className="mt-5 flex w-[350px] flex-col items-center rounded-md border-[1px]  border-[#ddd] bg-[#fffdfd] py-5 shadow-md">
+            <Text className="text-center font-medium">
+              HandShake: For early career job hunters and seekers
+            </Text>
+            <Text className="mt-3  tracking-wide">
+              If you are a recent graduate looking for an additional job
+              opportunity, or you represent an organization seeking to hire
+              young talent. Please visit the university employer connection
+              system
+            </Text>
+
+            <TouchableOpacity
+              onPress={() => Linking.openURL("https://www.alueducation.com/")}
+            >
+              <Text className="mt-4 w-[120px] bg-purple-500 px-4 py-2 text-center text-base text-white">
+                HandShake
+              </Text>
+            </TouchableOpacity>
+          </View>
         </View>
-        <View className="mt-5 flex flex-col  pl-5">
+
+        <Text className="mt-5 pl-5 text-base font-medium">Available Jobs</Text>
+
+        <View className="mt-1 flex flex-col  pl-5">
           {jobsRouter.data?.map((job) => (
             <JobPost post={job} key={job.id} />
           ))}
