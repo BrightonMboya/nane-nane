ae07d8ad4cefb99db5cb67ab523be73f204ebcc1
BrightonMboya <b.mboya@alustudent.com>
2023-06-01 11:36:24 +0200

    handled errors on verify email

--- apps/expo/src/components/Signup.tsx
+++ apps/expo/src/components/Signup.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import { Button, Text, TextInput, TouchableOpacity, View } from "react-native";
+import { Text, TextInput, TouchableOpacity, View } from "react-native";
 import { useSignUp } from "@clerk/clerk-expo";
 
 export default function SignUpScreen() {
@@ -10,6 +10,7 @@ export default function SignUpScreen() {
   const [pendingVerification, setPendingVerification] = React.useState(false);
   const [code, setCode] = React.useState("");
   const [error, setError] = React.useState("");
+  const [verifyEmailError, setVerifyEmailError] = React.useState("");
   // start the sign up process.
   const onSignUpPress = async () => {
     if (!isLoaded) {
@@ -46,7 +47,8 @@ export default function SignUpScreen() {
 
       await setActive({ session: completeSignUp.createdSessionId });
     } catch (err: any) {
-      console.error(JSON.stringify(err, null, 2));
+      // console.error(JSON.stringify(err, null, 2));
+      setVerifyEmailError(err.errors[0].longMessage);
     }
   };
 
@@ -98,17 +100,40 @@ export default function SignUpScreen() {
         </View>
       )}
       {pendingVerification && (
-        <View>
+        <View className="flex h-screen flex-col items-center justify-center bg-[#f2f2f2]">
           <View>
-            <TextInput
-              value={code}
-              placeholder="Code..."
-              onChangeText={(code) => setCode(code)}
-            />
+            <Text className="text-lg font-bold">
+              Enter the code sent to your email address
+            </Text>
+            <Text className="pt-1 text-center text-sm font-light">
+              Check your spam folder if you don't see the email.
+            </Text>
+
+            {verifyEmailError && (
+              <View>
+                <Text className="mt-5 font-medium text-red-500">
+                  {verifyEmailError}
+                </Text>
+              </View>
+            )}
+
+            <View className="flex flex-row items-center justify-center gap-5 pt-[40px]">
+              <TextInput
+                value={code}
+                placeholder="Code..."
+                onChangeText={(code) => setCode(code)}
+                placeholderTextColor="#383838"
+                className="w-[150px] border-b-[1px] border-[#383838] py-2"
+              />
+              <TouchableOpacity onPress={onPressVerify}>
+                <View className="rounded-md bg-indigo-500 ">
+                  <Text className="px-4 py-2 text-center font-medium text-white">
+                    Verify Email
+                  </Text>
+                </View>
+              </TouchableOpacity>
+            </View>
           </View>
-          <TouchableOpacity onPress={onPressVerify}>
-            <Text>Verify Email</Text>
-          </TouchableOpacity>
         </View>
       )}
     </View>
