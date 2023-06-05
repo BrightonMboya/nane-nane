524fd70c880dc0c0a420d13ffaa288c7d38bf433
BrightonMboya <b.mboya@alustudent.com>
2023-06-05 15:07:15 +0200

    added the keyboardavoidingView component

--- apps/expo/src/app/resources/addJob.tsx
+++ apps/expo/src/app/resources/addJob.tsx
@@ -1,5 +1,12 @@
 import React from "react";
-import { Text, TextInput, TouchableHighlight, View } from "react-native";
+import {
+  KeyboardAvoidingView,
+  Platform,
+  Text,
+  TextInput,
+  TouchableHighlight,
+  View,
+} from "react-native";
 import { ScrollView } from "react-native-gesture-handler";
 import { SafeAreaView } from "react-native-safe-area-context";
 
@@ -28,76 +35,81 @@ const AddJob = () => {
 
   return (
     <SafeAreaView>
-      <ScrollView className="pl-3">
-        <Text className="mt-[60px] text-xl font-bold">
-          Let's Create your job post
-        </Text>
-        <Text className="pt-2 text-sm">All fields are required</Text>
+      <KeyboardAvoidingView
+        behavior={Platform.OS === "ios" ? "padding" : "height"}
+        keyboardVerticalOffset={100}
+      >
+        <ScrollView className="h-full pl-3">
+          <Text className="mt-[60px] text-xl font-bold">
+            Let's Create your job post
+          </Text>
+          <Text className="pt-2 text-sm">All fields are required</Text>
 
-        <View className="flex flex-col gap-5 pt-[40px]">
-          <View className="mt-5 ">
-            <Text className={titleClassName}>Job Title</Text>
-            <TextInput
-              placeholder="Add the job Title"
-              value={form.title}
-              placeholderTextColor="#000"
-              className={inputClassName}
-              onChangeText={(text) => setForm({ ...form, title: text })}
-            />
+          <View className="flex flex-col gap-5 pt-[40px]">
+            <View className="mt-5 ">
+              <Text className={titleClassName}>Job Title</Text>
+              <TextInput
+                placeholder="Add the job Title"
+                value={form.title}
+                placeholderTextColor="#000"
+                className={inputClassName}
+                onChangeText={(text) => setForm({ ...form, title: text })}
+              />
+            </View>
+
+            <View>
+              <Text className={titleClassName}>Company</Text>
+              <TextInput
+                placeholder="Google Inc"
+                value={form.company}
+                onChangeText={(text) => setForm({ ...form, company: text })}
+                className={inputClassName}
+                placeholderTextColor="#000"
+              />
+            </View>
+
+            <View>
+              <Text className={titleClassName}>Location</Text>
+              <TextInput
+                placeholder="Kigali, Rwanda"
+                value={form.location}
+                onChangeText={(text) => setForm({ ...form, location: text })}
+                className={inputClassName}
+                placeholderTextColor="#000"
+              />
+            </View>
+
+            <View>
+              <Text className={titleClassName}>Job Description</Text>
+              <TextInput
+                placeholder="Add the job Description"
+                numberOfLines={40}
+                value={form.description}
+                onChangeText={(text) => setForm({ ...form, description: text })}
+                className={inputClassName}
+                placeholderTextColor="#000"
+              />
+            </View>
+
+            <View>
+              <Text className={titleClassName}>External Link</Text>
+              <TextInput
+                placeholder="Add link for application"
+                value={form.link}
+                onChangeText={(text) => setForm({ ...form, link: text })}
+                className={inputClassName}
+                placeholderTextColor="#000"
+              />
+            </View>
+
+            <TouchableHighlight onPress={handleSubmit}>
+              <Text className="rounded-base mt-3 w-[170px] bg-indigo-500 px-1  py-2  text-center text-base text-white">
+                Share Opportunity!
+              </Text>
+            </TouchableHighlight>
           </View>
-
-          <View>
-            <Text className={titleClassName}>Company</Text>
-            <TextInput
-              placeholder="Google Inc"
-              value={form.company}
-              onChangeText={(text) => setForm({ ...form, company: text })}
-              className={inputClassName}
-              placeholderTextColor="#000"
-            />
-          </View>
-
-          <View>
-            <Text className={titleClassName}>Location</Text>
-            <TextInput
-              placeholder="Kigali, Rwanda"
-              value={form.location}
-              onChangeText={(text) => setForm({ ...form, location: text })}
-              className={inputClassName}
-              placeholderTextColor="#000"
-            />
-          </View>
-
-          <View>
-            <Text className={titleClassName}>Job Description</Text>
-            <TextInput
-              placeholder="Add the job Description"
-              numberOfLines={40}
-              value={form.description}
-              onChangeText={(text) => setForm({ ...form, description: text })}
-              className={inputClassName}
-              placeholderTextColor="#000"
-            />
-          </View>
-
-          <View>
-            <Text className={titleClassName}>External Link</Text>
-            <TextInput
-              placeholder="Add link for application"
-              value={form.link}
-              onChangeText={(text) => setForm({ ...form, link: text })}
-              className={inputClassName}
-              placeholderTextColor="#000"
-            />
-          </View>
-
-          <TouchableHighlight onPress={handleSubmit}>
-            <Text className="rounded-base mt-3 w-[170px] bg-indigo-500 px-1  py-2  text-center text-base text-white">
-              Share Opportunity!
-            </Text>
-          </TouchableHighlight>
-        </View>
-      </ScrollView>
+        </ScrollView>
+      </KeyboardAvoidingView>
     </SafeAreaView>
   );
 };
