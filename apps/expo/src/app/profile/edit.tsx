cff276657ec76a80a696f32300dab9283e4147a2
BrightonMboya <b.mboya@alustudent.com>
2023-06-07 15:49:40 +0200

    added new schema on the users table

--- apps/expo/src/app/profile/edit.tsx
+++ apps/expo/src/app/profile/edit.tsx
@@ -1,5 +1,6 @@
+import React from "react";
 import { Image, ScrollView, Text, View } from "react-native";
-import { TouchableOpacity } from "react-native-gesture-handler";
+import { TextInput, TouchableOpacity } from "react-native-gesture-handler";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { Stack } from "expo-router";
 import { useAuth, useUser } from "@clerk/clerk-expo";
@@ -7,13 +8,9 @@ import { useAuth, useUser } from "@clerk/clerk-expo";
 import { api } from "~/utils/api";
 
 const profileData = {
-  name: "Asha Bonge",
   currentRole: "Software Engineer",
   location: "London, UK",
   classOf: "2019",
-  about: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam voluptatum, quibusdam, quia, quos voluptates voluptate voluptatibus quas doloribus quidem voluptatem. Quisquam voluptatum`,
-  profilePic:
-    "https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTB8fGF2YXRhcnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60.jpg",
   mentoringAread: ["Software Engineering", "Product Management", "UX Design"],
   experience: [
     {
@@ -34,8 +31,9 @@ const Index = () => {
   const { data } = api.users.profile.useQuery({
     email: user?.primaryEmailAddress?.emailAddress as string,
   });
-  console.log(data, "data");
-  const { signOut } = useAuth();
+
+  const [userName, setUserName] = React.useState(data?.username);
+
   return (
     <SafeAreaView className="relative bg-[#f2f2f2]">
       <Stack.Screen
@@ -52,16 +50,34 @@ const Index = () => {
               }}
               className="absolute left-[37%] top-[-30px] h-20 w-20 rounded-full"
             />
-            <View className="mt-[60px] flex flex-col items-center">
-              <Text className="text-lg">{data?.username}</Text>
+            <View className="mt-[60px] pl-5">
+              <View className="flex flex-row gap-3">
+                <Text className="text-lg">Name</Text>
+                <TextInput
+                  className="w-[250px] rounded-md border-b-[1px] border-b-[#ddd] px-3 py-1"
+                  value={userName as string}
+                  onChange={(text) => {
+                    setUserName(text as unknown as string);
+                  }}
+                />
+              </View>
+
+              {/* <View className="flex flex-row gap-3">
+                <Text className="text-lg">Name</Text>
+                <TextInput
+                  className="w-[250px] rounded-md border-b-[1px] border-b-[#ddd] px-3 py-1"
+                  value={userName as string}
+                  onChange={(text) => {
+                    setUserName(text as unknown as string);
+                  }}
+                />
+              </View> */}
+
               <Text>{profileData.currentRole}</Text>
               <View className="flex flex-row gap-3 pt-[10px]">
                 <Text>{profileData.location}</Text>
                 <Text>{`Class of ${profileData.classOf}`}</Text>
               </View>
-              <TouchableOpacity onPress={() => signOut()}>
-                <Text className="pt-2 font-medium text-blue-500">Sign Out</Text>
-              </TouchableOpacity>
             </View>
           </View>
 
