eebe16d6c078b6b83ee644f71986ebce9bc5ba81
BrightonMboya <b.mboya@alustudent.com>
2023-06-07 19:10:41 +0200

    corrected git error

--- packages/api/src/root.ts
+++ packages/api/src/root.ts
@@ -3,12 +3,14 @@ import { postRouter } from "./router/post";
 import { chatRouter } from "./router/chat";
 import { resourcesRouter } from "./router/resources";
 import { createTRPCRouter } from "./trpc";
+import { userRouter } from "./router/users";
 
 export const appRouter = createTRPCRouter({
   post: postRouter,
   auth: authRouter,
   resources: resourcesRouter,
   chat: chatRouter,
+  users: userRouter,
 });
 
 // export type definition of API
