389224665b4d8de460fde44280b9a29fcfa1a215
BrightonMboya <b.mboya@alustudent.com>
2023-07-03 21:43:23 +0200

    fixing the monorepo cache

--- apps/expo/app.config.ts
+++ apps/expo/app.config.ts
@@ -1,8 +1,8 @@
 import type { ExpoConfig } from "@expo/config";
 
 const defineConfig = (): ExpoConfig => ({
-  name: "expo",
-  slug: "expo",
+  name: "nane_nane",
+  slug: "nane_nane",
   scheme: "expo",
   version: "1.0.0",
   orientation: "portrait",
@@ -22,6 +22,8 @@ const defineConfig = (): ExpoConfig => ({
     bundleIdentifier: "your.bundle.identifier",
   },
   android: {
+    package: "com.mboya.nane_nane",
+    versionCode: 1,
     adaptiveIcon: {
       foregroundImage: "./assets/logo.jpeg",
       backgroundColor: "#f6f6f6",
