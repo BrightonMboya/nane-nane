c13464bd13ce56efaa14341cb2043f0d1dcac60e
BrightonMboya <b.mboya@alustudent.com>
2023-07-04 10:00:20 +0200

    healed with expo-doctor

--- apps/expo/app.config.ts
+++ apps/expo/app.config.ts
@@ -1,15 +1,15 @@
 import type { ExpoConfig } from "@expo/config";
 
 const defineConfig = (): ExpoConfig => ({
-  name: "nane_nane",
-  slug: "nane_nane",
+  name: "nane-nane",
+  slug: "nane-nane",
   scheme: "expo",
   version: "1.0.0",
   orientation: "portrait",
-  icon: "./assets/logo.jpeg",
+  icon: "./assets/icon.png",
   userInterfaceStyle: "light",
   splash: {
-    image: "./assets/logo.jpeg",
+    image: "./assets/icon.png",
     resizeMode: "contain",
     backgroundColor: "#f6f6f6",
   },
@@ -25,13 +25,14 @@ const defineConfig = (): ExpoConfig => ({
     package: "com.mboya.nane_nane",
     versionCode: 1,
     adaptiveIcon: {
-      foregroundImage: "./assets/logo.jpeg",
+      foregroundImage: "./assets/icon.png",
       backgroundColor: "#f6f6f6",
     },
   },
   extra: {
     eas: {
-      "projectId": "fe627e60-92a5-4ddd-a3b2-93ae82d43d2f"
+      "projectId": "478d896b-e83d-4301-9564-e9ba9db63786"
+
     },
   },
   plugins: ["./expo-plugins/with-modify-gradle.js"],
