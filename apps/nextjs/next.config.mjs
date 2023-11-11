1a7ccac58bb8a8fcd790f8f1b2740f7f64082701
brightonmboya <b.mboya@alustudent.com>
2023-11-11 16:11:06 +0200

    configured the pwa stuff

--- apps/nextjs/next.config.mjs
+++ apps/nextjs/next.config.mjs
@@ -1,7 +1,30 @@
 // Importing env files here to validate on build
 import "./src/env.mjs";
 import "@repo/auth/env.mjs";
-import withPWA from 'next-pwa';
+import nextPWA from 'next-pwa'
+// import withPWA from 'next-pwa';
+
+const withPWA = nextPWA({
+  dest: 'public',
+  register: true,
+  skipWaiting: true,
+  disable: process.env.NODE_ENV === 'development',
+})
+
+// module.exports = withPWA({
+//   reactStrictMode: true,
+//   /** Enables hot reloading for local packages without a build step */
+//   transpilePackages: ["@repo/api", "@repo/auth", "@repo/db", "@repo/ui"],
+//   /** We already do linting and typechecking as separate tasks in CI */
+//   eslint: { ignoreDuringBuilds: true },
+//   typescript: { ignoreBuildErrors: true },
+//   images: {
+//     domains: [
+//       "images.unsplash.com",
+//       "cdn.sanity.io",
+//     ],
+//   },
+// })
 
 /** @type {import("next").NextConfig} */
 const config = withPWA({
@@ -17,12 +40,7 @@ const config = withPWA({
       "cdn.sanity.io",
     ],
   },
-  pwa: {
-    dest: "public",
-    register: true,
-    skipWaiting: true,
-    disable: process.env.NODE_ENV === 'development',
-  },
+ 
 });
 
 export default config;
