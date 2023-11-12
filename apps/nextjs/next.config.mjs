0ad25bd665da6872a69ec26e8628cc6babe58a1f
brightonmboya <b.mboya@alustudent.com>
2023-11-12 12:54:24 +0200

    looks like pwa now works

--- apps/nextjs/next.config.mjs
+++ apps/nextjs/next.config.mjs
@@ -8,7 +8,7 @@ const withPWA = nextPWA({
   dest: 'public',
   register: true,
   skipWaiting: true,
-  disable: process.env.NODE_ENV === 'development',
+  // disable: process.env.NODE_ENV === 'development',
 })
 
 // module.exports = withPWA({
